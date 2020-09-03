e5c458e31a04927029af136702074f394d12fbbb
Decket <gatezhdesket@gmail.com>
2020-09-03 20:14:10 +0300

    JS-Obfuscator

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var fs = require('fs');
 var JSObfuscator = require('javascript-obfuscator');
 var dateFormat = require('dateformat');
+var path = require('path');
+var async = require('async');
 var now = new Date();
 now = dateFormat(now, "dd-mm-yyyy_ss-MM-hh");
 
@@ -10,6 +12,78 @@ var config = {
     output_dir: `${__dirname}/output/${now}/`
 }
 
+function getFiles (dirPath, callback) {
+
+    fs.readdir(dirPath, function (err, files) {
+        if (err) return callback(err);
+
+        var filePaths = [];
+        async.eachSeries(files, function (fileName, eachCallback) {
+            var filePath = path.join(dirPath, fileName);
+
+            fs.stat(filePath, function (err, stat) {
+                if (err) return eachCallback(err);
+
+                if (stat.isDirectory()) {
+                    getFiles(filePath, function (err, subDirFiles) {
+                        if (err) return eachCallback(err);
+
+                        filePaths = filePaths.concat(subDirFiles);
+                        eachCallback(null);
+                    });
+
+                } else {
+                    if (stat.isFile() && /\.js$/.test(filePath)) {
+                        filePaths.push(filePath);
+                    }
+
+                    eachCallback(null);
+                }
+            });
+        }, function (err) {
+            callback(err, filePaths);
+        });
+
+    });
+}
+
+
+getFiles(config.input_dir, function (err, jsfile) {
+    if (err) return console.log(err);
+    if(jsfile.length <= 0) return console.warn("Файлы не найдены!");
+
+    jsfile.forEach((f, i) => {
+        fs.readFile(__dirname + '/' + f, "UTF-8", (err, data) => {
+            if(err) throw err;
+
+            f_ = f.split('\\').splice(1, 5000);
+            delete f_[f_.length-1];
+            f_ = f_.join('\\');
+            
+            fs.mkdir(config.output_dir + '/' + f_, {recursive: true}, (err) => {
+                if(err) return console.log(err);
+
+                var Result = JSObfuscator.obfuscate(data,
+                            {
+                                compact: false,
+                                controlFlowFlattening: true,
+                                numbersToExpressions: true,
+                                simplify: true,
+                                shuffleStringArray: true,
+                                splitStrings: true
+                            });
+                f = f.split('\\').splice(1, 5000).join('\\');
+                fs.writeFile(config.output_dir + f, Result.getObfuscatedCode(), (err) => {
+                    if(err) return console.log(err);
+                    console.log(`File was saved! ${config.output_dir + f}`)
+                });
+            });
+        });
+    });
+});
+
+
+/*
 fs.readdir(config.input_dir, (err, files) => {
     if(err) return console.error(err);
   
@@ -45,3 +119,4 @@ fs.readdir(config.input_dir, (err, files) => {
     });
   
 });
+*/
--- input/test.js
+++ input/test.js
@@ -1,12 +1,76 @@
-(function(){
-      var variable1 = '5' - 3;
-      var variable2 = '5' + 3;
-      var variable3 = '5' + - '2';
-      var variable4 = ['10','10','10','10','10'].map(parseInt);
-      var variable5 = 'foo ' + 1 + 1;
-      console.log(variable1);
-      console.log(variable2);
-      console.log(variable3);
-      console.log(variable4);
-      console.log(variable5);
-  })();
+var _0x1a4e = [
+    'log',
+    'wmhjh',
+    '5|3|4|7|1',
+    'yeExG',
+    'foo\x20',
+    '2|8|0|6|9|',
+    'map',
+    'flPMF'
+];
+(function (_0x23b0c9, _0x522c65) {
+    var _0x5cc5e3 = function (_0x8861d0) {
+        while (--_0x8861d0) {
+            _0x23b0c9['push'](_0x23b0c9['shift']());
+        }
+    };
+    _0x5cc5e3(++_0x522c65);
+}(_0x1a4e, 0x62c + -0x41 * 0x45 + 0xca5));
+var _0xeb26 = function (_0x23b0c9, _0x522c65) {
+    _0x23b0c9 = _0x23b0c9 - (0x62c + -0x41 * 0x45 + 0xb59);
+    var _0x5cc5e3 = _0x1a4e[_0x23b0c9];
+    return _0x5cc5e3;
+};
+(function () {
+    var _0x12d249 = {
+            'qTvAA': _0xeb26('0x1') + _0xeb26('0x6'),
+            'wmhjh': function (_0x1de60b, _0x4ae5ab) {
+                return _0x1de60b + _0x4ae5ab;
+            },
+            'flPMF': function (_0x1b418b, _0x197b79) {
+                return _0x1b418b + _0x197b79;
+            },
+            'yeExG': _0xeb26('0x0')
+        }, _0x35ee33 = _0x12d249['qTvAA']['split']('|'), _0x176120 = -0x25bc + -0x239a + 0x4956;
+    while (!![]) {
+        switch (_0x35ee33[_0x176120++]) {
+        case '0':
+            var _0xcdc350 = _0x12d249[_0xeb26('0x5')]('5', -'2');
+            continue;
+        case '1':
+            console['log'](_0x1332bd);
+            continue;
+        case '2':
+            var _0x57b6e0 = '5' - (-0x192e + -0x1 * -0x1ef + 0x1742);
+            continue;
+        case '3':
+            console[_0xeb26('0x4')](_0x10d573);
+            continue;
+        case '4':
+            console[_0xeb26('0x4')](_0xcdc350);
+            continue;
+        case '5':
+            console[_0xeb26('0x4')](_0x57b6e0);
+            continue;
+        case '6':
+            var _0x120071 = [
+                '10',
+                '10',
+                '10',
+                '10',
+                '10'
+            ][_0xeb26('0x2')](parseInt);
+            continue;
+        case '7':
+            console['log'](_0x120071);
+            continue;
+        case '8':
+            var _0x10d573 = _0x12d249[_0xeb26('0x3')]('5', -0xe0a + -0xdaa + 0x93d * 0x3);
+            continue;
+        case '9':
+            var _0x1332bd = _0x12d249[_0xeb26('0x3')](_0x12d249[_0xeb26('0x3')](_0x12d249[_0xeb26('0x7')], 0x2106 + -0x1591 + -0x1 * 0xb74), -0x571 + 0x710 + -0x19e);
+            continue;
+        }
+        break;
+    }
+}());
